fb0577f9227512c0dc6a7a3d1d545dd9db64a362
Michael07220823 <michael31703@gmail.com>
2021-08-14 06:42:54 +0800

    Update version to 0.0.8.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as readme:
 
 setuptools.setup(
     name="new_tools",
-    version="0.0.7",
+    version="0.0.8",
     author="Overcomer",
     author_email="michael31703@gmail.com",
     description="Multiple tools",
